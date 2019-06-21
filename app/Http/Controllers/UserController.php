6359c61a79426e78397c05e5b34ab07b7e543c18
John Feitosa <joaohenriquefsf@gmail.com>
2019-06-21 19:50:56 -0300

    conclusao

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
         $this->validate($request, [
             'name' => 'required',
             'email' => 'required|unique:users|max:255',
-            'password' => 'required:max:255'
+            'password' => 'required|max:255|confirmed'
         ]);
 
         $user = new User($request->all());
@@ -25,17 +25,29 @@ class UserController extends Controller
     }
 
     public function update(Request $request, $id){
-        $this->validate($request, [
+        $dadosValidacao = [
             'name' => 'required',
-            'email' => 'required|unique:users|max:255',
-            'password' => 'required:max:255'
-        ]);
+            'email' => 'required|unique:users|max:255'
+        ];
+
+        if(isset($request->all()['password'])){
+            if($request->all()['password'] == '' || $request->all()['password'] == null){
+                unset($request->all()['password']);
+            }else{
+                $dadosValidacao['password'] = 'required|max:255|confirmed';
+            }
+            
+        }
+
+        $this->validate($request, $dadosValidacao);
 
         $user = User::find($id);
 
         $user->name = $request->input('name');
         $user->email = $request->input('email');
-        $user->password = $request->input('password');
+        if(isset($request->all()['password'])){
+            $user->password = $request->input('password');
+        }
         $user->update();
 
         return $user;
@@ -44,6 +56,18 @@ class UserController extends Controller
 
     public function view($id){
         return User::find($id);
+    }
+
+    public function list(){
+        return User::all();
+    }
+
+    public function delete($id){
+        if(User::destroy($id)){
+            return response('Removido com sucesso.', 200);
+        }else{
+            return response('Erro ao remover.', 401);
+        }
     }
 
 }
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -14,13 +14,16 @@ class UserTeste extends TestCase
     public function testCreateUser()
     {
         $dados = [
-            'name'      => 'Nome 01',
-            'email'     => 'email@exemplo.com',
-            'password'  => '123'
+            'name'                   => 'Nome 01',
+            'email'                  => 'email@exemplo.com',
+            'password'               => '123',
+            'password_confirmation'  => '123'
         ];
 
         $this->post('/api/user', $dados);
         $this->assertResponseOk();
+
+        
 
         $resposta = (array) json_decode($this->response->content());
 
@@ -43,14 +46,46 @@ class UserTeste extends TestCase
         $this->assertArrayHasKey('id', $resposta);
     }
 
-    public function testUpdateUser()
+    public function testUpdateWithPasswordUser()
     {
         $user = \App\User::first();
 
         $dados = [
-            'name'      => 'Nome 02'.date('Ymdis').' '.rand(1,100),
-            'email'     => 'email4@exemplo.com',
-            'password'  => '123'
+            'name'                   => 'Nome 02'.date('Ymdis').' '.rand(1,100),
+            'email'                  => 'email4@exemplo.com',
+            'password'               => '123',
+            'password_confirmation'  => '123'
+        ];
+
+        $this->put('/api/user/'.$user->id, $dados);
+
+
+        $this->assertResponseOk();
+
+        $resposta = (array) json_decode($this->response->content());
+
+        $this->assertArrayHasKey('name', $resposta);
+        $this->assertArrayHasKey('email', $resposta);
+        $this->assertArrayHasKey('id', $resposta);
+
+        $this->seeInDatabase('users', [
+            'name'  => $dados['name'],
+            'email' => $dados['email']
+        ]);
+
+        $this->notSeeInDatabase('users', [
+            'name'  => $user->name,
+            'email' => $user->email
+        ]);
+    }
+
+    public function testUpdateWithoutPasswordUser()
+    {
+        $user = \App\User::first();
+
+        $dados = [
+            'name'                   => 'Nome 02'.date('Ymdis').' '.rand(1,100),
+            'email'                  => 'email4@exemplo.com'
         ];
 
         $this->put('/api/user/'.$user->id, $dados);
@@ -72,5 +107,27 @@ class UserTeste extends TestCase
             'name'  => $user->name,
             'email' => $user->email
         ]);
+    }
+
+    public function testAllUser()
+    {
+        $this->get('/api/users');
+        $this->assertResponseOk();
+        $this->seeJsonStructure([
+            '*' => [
+                'id',
+                'name',
+                'email'
+            ]
+        ]);
+    }
+
+    public function testDeleteUser()
+    {
+        $user = \App\User::first();
+
+        $this->delete('/api/user/'.$user->id);
+        $this->assertResponseOk();
+        $this->assertEquals('Removido com sucesso.', $this->response->content());
     }
 }
